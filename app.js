a1e252ca65c49936895af5e5a204e20610fb67a3
Mathu <z5419643@ad.unsw.edu.au>
2025-08-24 14:22:11 +1000

    added configs

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import express from "express";
 
 const app = express();
 
+import { PORT } from "./config/env.js";
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(3000, () => {
-  console.log("Subscription Tracker API is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
 });
 
 export default app;
--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // keep browser globals
+        ...globals.node, // add Node.js globals (process, __dirname, etc.)
+      },
+    },
+  },
 ]);
