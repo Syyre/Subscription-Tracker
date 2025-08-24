1fef4917ee1abdfbba114ef32703f22f61d3d1c5
Mathu <z5419643@ad.unsw.edu.au>
2025-08-24 13:29:47 +1000

    set up express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("server running on port 3000");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
+
+app.listen(3000, () => {
+  console.log("Subscription Tracker API is running on http://localhost:3000");
+});
+
+export default app;
