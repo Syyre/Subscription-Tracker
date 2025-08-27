422d029b63a7723f4c47b1397d520028bff15b4f
Mathu <z5419643@ad.unsw.edu.au>
2025-08-27 13:09:39 +1000

    connected with mongodb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 import { PORT } from "./config/env.js";
 
+import connectToDatabase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -20,6 +21,8 @@ app.listen(PORT, () => {
   console.log(
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
+
+  connectToDatabase();
 });
 
 export default app;
