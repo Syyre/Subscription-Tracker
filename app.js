751663b92549682fc577ad5c62baf759ab0d40fc
Mathu <z5419643@ad.unsw.edu.au>
2025-08-27 14:10:48 +1000

    Added error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 import { PORT } from "./config/env.js";
 
@@ -8,6 +11,8 @@ import connectToDatabase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
@@ -16,12 +21,12 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
-
   connectToDatabase();
 });
 
